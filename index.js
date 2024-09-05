124ff24059705d3ff7d71a2366f9d5f56782afed
Daniil <temnay2@icloud.com>
2024-09-05 23:13:47 +0300

    add https

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const router = require('./router/index');
 const errorMiddleware = require('./middlewares/error-middleware');
+const fs = require('fs');
+const https = require('https');
 
 const PORT = process.env.PORT || 5050;
+const mode = process.env.MODE || 'development';
 const app = express();
 
 app.use(express.json());
@@ -22,7 +25,23 @@ app.use(errorMiddleware);
 const start = async () => {
     try {
         await mongoose.connect(process.env.DB_URL);
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+        if (process.env.MODE === 'production') {
+            // В продакшн используем HTTPS
+            const options = {
+                key: fs.readFileSync('/etc/letsencrypt/live/unialbums.ru/privkey.pem'),
+                cert: fs.readFileSync('/etc/letsencrypt/live/unialbums.ru/fullchain.pem')
+            };
+
+            https.createServer(options, app).listen(PORT, () => {
+                console.log(`HTTPS сервер запущен на порту ${PORT}`);
+            });
+        } else {
+            // Локальная разработка с HTTP
+            app.listen(PORT, () => {
+                console.log(`HTTP сервер запущен на порту ${PORT}`);
+            });
+        }
     } catch (e) {
         console.log(e);
     }
